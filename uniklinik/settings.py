7f291e71e477afb8cbfa20654563c26485b500a0
Mimoun Bijjou <mimounbijjou@Mimouns-MacBook-Air.fritz.box>
2019-02-24 03:28:49 +0100

    remove token

--- uniklinik/settings.py
+++ uniklinik/settings.py
@@ -166,8 +166,8 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+        #'rest_framework.authentication.TokenAuthentication',  # <-- And here
+        #'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ]
 }
 
