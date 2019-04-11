19b122e6ff1bc07eb94d946d66816a94f1bce866
Mimoun Bijjou <mimounbijjou@Mimouns-Air.fritz.box>
2019-04-11 22:47:55 +0200

    add endpoint

--- subject_area/api_urls.py
+++ subject_area/api_urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-
+from rest_framework.routers import DefaultRouter
 from subject_area.datatables import SubjectAreaDatatables
+from subject_area.viewsets import SubjectAreaViewSet
+from django.urls import include
+
+
+router = DefaultRouter(trailing_slash=False)
+router.register("subject-areas", SubjectAreaViewSet)
+
 
 urlpatterns = [
+    path(r'', include(router.urls)),
     path(r'subject-areas-datatables/', SubjectAreaDatatables.as_view(), name="subject_area_datatables"),
 ]
