0a91fac4625be1a70db7859c5a2a65ce62bf7d41
Mimoun Bijjou <mimounbijjou@Mimouns-Air.local>
2019-05-13 05:07:41 +0200

    bool in tasks

--- taskmanagement/serializers.py
+++ taskmanagement/serializers.py
@@ -13,18 +13,41 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('username', 'first_name', 'last_name', "email", "is_superuser", "profile", )
 
 
-class UserTaskSerializer(serializers.HyperlinkedModelSerializer):
-    user = UserSerializer()
+class FilteredListSerializer(serializers.ListSerializer):
+    def to_representation(self, data):
+        user_id = self.context.get('user_id')
+        if user_id:
+            data = data.filter(user=self.context.get("user_id"))
+            return data.values("completed").first()
+        else:
+            return data.values("user", "completed")
 
+
+class UserTaskSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = UserTask
-        fields = ("pk", "user", "completed")
+        list_serializer_class = FilteredListSerializer
+        fields = ("user", "completed", )
 
 
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
+    def to_representation(self, obj):
+        primitive_repr = super().to_representation(obj)
+        user_id = self.context.get("user_id")
+        if user_id:
+            primitive_repr['completed'] = primitive_repr['usertasks']["completed"]
+            primitive_repr.pop("usertasks")
+        else:
+            primitive_repr['completed'] = primitive_repr['usertasks']
+            primitive_repr.pop("usertasks")
+
+        return primitive_repr
+
+    usertasks = UserTaskSerializer(read_only=True, many=True)
+
     class Meta:
         model = Task
-        fields = ("pk", "name", "start_datetime", "end_datetime", "description",)
+        fields = ("pk", "name", "start_datetime", "end_datetime", "description", "usertasks",)
 
 
 class EndUserTaskSerializer(serializers.ModelSerializer):
