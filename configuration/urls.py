06acdca40c0398f54d8aed2178848dc5a450915a
Mimoun Bijjou <mimounbijjou@Mimouns-Air.fritz.box>
2019-08-22 23:07:33 +0200

    dynamic static url

--- configuration/urls.py
+++ configuration/urls.py
@@ -1,8 +1,9 @@
-from configuration.views import configuration_view
+from configuration.views import configuration_view, logo_view
 from django.urls import path
 
 
 urlpatterns = [
     path(r'', configuration_view, name="config"),
+    path(r'logo', logo_view, name="logo"),
 
 ]
