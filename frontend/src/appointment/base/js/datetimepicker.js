c78fb82596fe46332adb6544b65ec8cabc12aef2
Mimoun Bijjou <mimounbijjou@Mimouns-Air.local>
2019-08-28 05:44:14 +0200

    new form start_date and end_date fixed

--- frontend/src/appointment/base/js/datetimepicker.js
+++ frontend/src/appointment/base/js/datetimepicker.js
@@ -1,5 +1,6 @@
 // datetimepicker = require("~/vendor/bootstrap-datetimepicker/4.17.47/build/js/bootstrap-datetimepicker.min.js")
 
+
 $(document).ready(function(){
         let config = {
             locale: 'de',
@@ -16,11 +17,21 @@ $(document).ready(function(){
 
         $('#id_end_date').datetimepicker(config);
 
+        let last_start_date_event = null;
+
         $("#id_start_date").on("dp.change", function (e) {
-            $('#id_end_date').data("DateTimePicker").minDate(e.date);
-            $('#id_end_date').data("DateTimePicker").defaultDate(e.date)
+            last_start_date_event = e;
+        });
+
+        $('#id_start_date').on("focusout", function (e) {
+            if(last_start_date_event){
+                $('#id_end_date').data("DateTimePicker").minDate(last_start_date_event.date);
+                $('#id_end_date').data("DateTimePicker").defaultDate(last_start_date_event.date);
+                last_start_date_event = null;
+            }
             $('#id_end_date').select()
         });
+
 
         $("#id_end_date").on("dp.change", function (e) {
             $('#id_start_date').data("DateTimePicker").maxDate(e.date);
