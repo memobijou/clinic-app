da5550b0d45a1d6f8849966c1b1d050f077125d6
mbijou <mimounbijjou@Mimouns-Air.fritz.box>
2020-02-28 09:43:51 +0100

    custom authentication token try

--- uniklinik/routing.py
+++ uniklinik/routing.py
@@ -3,12 +3,13 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
 from messaging.consumers import ChatConsumer
+from uniklinik.channels_auth import TokenAuthMiddlewareStack
 
 application = ProtocolTypeRouter({
     "websocket":
     # Empty for now (http->django views is added by default)
     AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
+        TokenAuthMiddlewareStack(
             URLRouter([
                 path('messaging/chat/<int:receiver>/<int:sender>/', ChatConsumer)
             ])
