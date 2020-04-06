560252849ef084f88778196b3016ae1a5a4bd32f
mbijou <mimounbijjou@Mimouns-Air.local>
2020-04-06 22:02:28 +0200

    url changed for duty-roster and broadcast needs user_id in query string

--- appointment/api_urls.py
+++ appointment/api_urls.py
@@ -7,9 +7,11 @@ appointment_router = routers.DefaultRouter()
 appointment_router.register(r'appointments', AppointmentViewSet, basename="appointment")
 
 duty_roster_router = routers.DefaultRouter()
-duty_roster_router.register(r'api', DutyRosterViewSet, basename="duty_roster")
+duty_roster_router.register(r'duty-rosters', DutyRosterViewSet, basename="duty_roster")
 
 
 urlpatterns = [
     path(r'users/<int:user_id>/', include(appointment_router.urls)),
+    path(r'users/<int:user_id>/', include(duty_roster_router.urls)),
+
 ]
